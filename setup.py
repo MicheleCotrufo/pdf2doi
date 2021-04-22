f1e7fcd862c3f21adf4832bd8096848c58e3298d
Michele Cotrufo <64863140+MicheleCotrufo@users.noreply.github.com>
2021-04-21 23:15:21 -0400

    fix version

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ with open("requirements.txt") as f:
     required_packages = f.read().splitlines()
 
 setuptools.setup(name='pdf2doi',
-      version='0.1.4a1',
-      description='A python library/command-line tool to retrieve the DOI of a paper from a pdf file.',
+      version='0.2',
+      description='A python library/command-line tool to retrieve the DOI or other identifiers (e.g. arXiv) from pdf files of a publications.',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/MicheleCotrufo/pdf2doi',

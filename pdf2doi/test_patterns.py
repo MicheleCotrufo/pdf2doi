ecf9bb1ce626d857d35cedbc2fdfd6ff1bd1e72d
DJRHails <daniel@hails.info>
2022-09-08 22:19:28 +0100

    Add test cases and statistics

--- pdf2doi/test_patterns.py
+++ pdf2doi/test_patterns.py
@@ -2,19 +2,60 @@ import pytest
 import re
 from pdf2doi.patterns import (
     doi_pattern,
-    doi_regexp
+    doi_regexp,
+    standardise_doi
 )
 
-@pytest.mark.parametrize("suspected", [
-    "10.1109/sp.2011.40",
-])
+BASIC_DOIS = [
+    "10.1006/jmrb.1993.1004",
+    "10.1068/p080244",
+    "10.2307/357448",
+    "10.1002/cber.19260590832",
+    "10.1016/0141-4607(85)90047-2",
+    "10.1016/j.gaitpost.2009.07.035",
+    "10.1109/sibgrapi.2012.5",
+    "10.1111/j.1532-5415.2012.04014.x",
+    "10.2307/3950104",
+    "10.1002/esp.3322"
+]
+
+DOIS_WITH_NON_STANDARD_SEPARTORS = [
+    doi.replace("/", ":") for doi in BASIC_DOIS
+] + [
+    doi.replace("/", " ") for doi in BASIC_DOIS
+] + [
+    f"[{doi.replace('/', ']')}" for doi in BASIC_DOIS
+]
+
+# Also includes those with dots
+DOIS_WITH_SHORT_NAMESPACES = [
+    "10.2.337/dc08-2337",
+    "10.58.12/numonthly.14189",
+    "10.16/j.reuma.2008.12.011"
+]
+
+STRANGE_BUT_VALID_DOIS = [
+    "10.1642/0004-8038(2005)122[0121:POTPIS]2.0.CO;2",
+    "10.1002/1521-4141(200106)31:6<1685::aid-immu1685>3.0.co;2-v",
+    "10.1676/0043-5643(2002)114[0197:rbacib]2.0.co;2",
+    "10.1061/(asce)0733-9429(2008)134:4(390)" # terminates in a closing parenthesis
+]
+
+@pytest.mark.parametrize("suspected", BASIC_DOIS)
 def test_is_strict_doi_match(suspected):
-    assert re.match(doi_pattern, suspected,re.I) is not None
+    assert re.match(doi_pattern, suspected, re.I) is not None
+
+@pytest.mark.parametrize(["suspected", "expected"], [
+    ["10.1177:0146167297234003", "10.1177/0146167297234003"],
+    *zip(DOIS_WITH_NON_STANDARD_SEPARTORS, BASIC_DOIS + BASIC_DOIS + BASIC_DOIS),
+])
+def test_standardise_doi(suspected, expected): 
+    assert standardise_doi(suspected) == expected
 
 @pytest.mark.parametrize(["suspected", "expected"], [
     ["10.1109/sp.2011.40"] * 2,
-    ["doi10.1177:0146167297234003", "10.1177:0146167297234003"],
-    ["10.1177:0146167297234003.pdf", "10.1177:0146167297234003"],
+    ["doi10.1177:0146167297234003", "doi10.1177:0146167297234003"],
+    ["10.1177:0146167297234003.pdf", "10.1177:0146167297234003.pdf"],
 ])
 def test_is_loose_doi_match(suspected, expected):
     print(suspected)
